d4b395b0744a2b2423e8508bb5675e167a32d0ff
”fullstackmalina” <malinkin@mail.ru>
2023-03-15 15:56:44 +0300

    first commit

---
+++ public/loginPage.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const userForm = new UserForm();
+
+
+userForm.loginFormCallback = function (loginData) {
+    let loginCallback = (loginResponse) => {
+        if (!loginResponse.success) {
+            this.setLoginErrorMessage('Ошибка при вводе данных или введенный вами идентификатор пользователя не существует!');
+        } else location.reload();
+    };
+    ApiConnector.login(loginData, loginCallback);
+}
+
+userForm.registerFormCallback = function (registerData) {
+    let registerCallback = (registerResponse) => {
+        if (!registerResponse.success) {
+            this.setRegisterErrorMessage('Данные введены не правильно, проверьте правильно ли вы указали свои данные!');
+        } else location.reload();
+    };
+    ApiConnector.register(registerData, registerCallback);
+}
